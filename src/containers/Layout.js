26a9f9b30786f5da6eadd4f1612c31d4e17081b6
allamatti98 <dayanallamatti98@gmail.com>
2022-11-06 13:58:49 +0300

    Rebase

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -15,60 +15,15 @@ class CustomLayout extends React.Component {
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={["5"]}
+            defaultSelectedKeys={["2"]}
             style={{ lineHeight: "64px" }}
           >
-            <Menu.Item key="1" onClick={this.props.logout}>
-              <Link to="/">Home</Link>
-            </Menu.Item>
-            <Menu.Item key="2" onClick={this.props.logout}>
-              <Link to="/">Courses</Link>
-            </Menu.Item>
-            <Menu.Item key="3" onClick={this.props.logout}>
-              <Link to="/">Blog</Link>
-            </Menu.Item>
-            <Menu.Item key="3" onClick={this.props.logout}>
-              <Link to="/">FAQ</Link>
-            </Menu.Item>
-            <Menu.Item key="4" onClick={this.props.logout}>
-              <Link to="/">Contact Us</Link>
-            </Menu.Item>
-            <Menu.Item key="3" onClick={this.props.logout}>
-              <Link to="/">Contribute</Link>
-            </Menu.Item>
-            <Menu.Item key="2" onClick={this.props.logout}>
-              <Link to="/">Gallery</Link>
-            </Menu.Item>
-            <Menu.Item key="3" onClick={this.props.logout}>
-              <Link to="/">Community</Link>
-            </Menu.Item>
-            <Menu.Item key="3" onClick={this.props.logout}>
-              <Link to="/">Teachers</Link>
-            </Menu.Item>
-            <Menu.Item key="3" onClick={this.props.logout}>
-              <Link to="/">Contests</Link>
-            </Menu.Item>
-            <Menu.Item key="3" onClick={this.props.logout}>
-              <Link to="/">Careers</Link>
-            </Menu.Item>
-            <Menu.Item key="2" onClick={this.props.logout}>
-              <Link to="/">Policies</Link>
-            </Menu.Item>
-            <Menu.Item key="2" onClick={this.props.logout}>
-              <Link to="/">Help</Link>
-            </Menu.Item>
-
-
-
             {this.props.isAuthenticated ? (
-              <Menu.Menu position="right" >
-                <Menu.Item key="5" onClick={this.props.logout}>
-                  Logout
-                </Menu.Item>
-              </Menu.Menu>
-
+              <Menu.Item key="2" onClick={this.props.logout}>
+                Logout
+              </Menu.Item>
             ) : (
-              <Menu.Item key="5">
+              <Menu.Item key="2">
                 <Link to="/login">Login</Link>
               </Menu.Item>
             )}
@@ -76,6 +31,9 @@ class CustomLayout extends React.Component {
         </Header>
         <Content style={{ padding: "0 50px" }}>
           <Breadcrumb style={{ margin: "16px 0" }}>
+            <Breadcrumb.Item>
+              <Link to="/">Home</Link>
+            </Breadcrumb.Item>
             {this.props.token !== null ? (
               <Breadcrumb.Item>
                 <Link to={`/profile/${this.props.userId}`}>Profile</Link>
