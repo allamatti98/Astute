90221ff38f314f9e76c0943fa3fe6d3c87d05100
allamatti98 <dayanallamatti98@gmail.com>
2022-11-06 17:10:21 +0300

    Contests positions

--- src/components/Contests.jsx
+++ src/components/Contests.jsx
@@ -1,15 +1,46 @@
-import { Card } from 'antd';
+import { Card, Col, Row } from 'antd';
 import React from 'react';
 const { Meta } = Card;
 const Conto = () => (
-    <Card
-        hoverable
-        style={{
-            width: 240,
-        }}
-        cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
-    >
-        <Meta title="Math Contest" description="20th November" />
-    </Card>
+    <div>
+        <Row gutter={16}>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="20th November" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Physics Contest" description="13th December" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="12th January" />
+                </Card>
+            </Col>
+        </Row>
+
+
+
+    </div >
 );
 export default Conto;
