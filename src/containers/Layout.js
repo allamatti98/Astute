2b1621a13217703298004dc0a450f7676d33ff19
allamatti98 <dayanallamatti98@gmail.com>
2022-11-06 14:47:43 +0300

    more changes

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -15,7 +15,7 @@ class CustomLayout extends React.Component {
           <Menu
             theme="dark"
             mode="horizontal"
-            defaultSelectedKeys={["2"]}
+            defaultSelectedKeys={["3"]}
             style={{ lineHeight: "64px" }}
           >
             <Menu.Item key="3" onClick={this.props.logout}>
