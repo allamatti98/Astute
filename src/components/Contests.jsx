00372bf94821ad5681d2c72e5b52b946046c75d0
allamatti98 <dayanallamatti98@gmail.com>
2022-11-06 17:12:34 +0300

    Contests edits

--- src/components/Contests.jsx
+++ src/components/Contests.jsx
@@ -38,9 +38,151 @@ const Conto = () => (
                 </Card>
             </Col>
         </Row>
-
-
-
+        <br /><br /><br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="20th November" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Physics Contest" description="13th December" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="12th January" />
+                </Card>
+            </Col>
+        </Row>
+        <br /><br /><br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="20th November" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Physics Contest" description="13th December" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="12th January" />
+                </Card>
+            </Col>
+        </Row>
+        <br /><br /><br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="20th November" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Physics Contest" description="13th December" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="12th January" />
+                </Card>
+            </Col>
+        </Row>
+        <br /><br /><br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="20th November" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Physics Contest" description="13th December" />
+                </Card>
+            </Col>
+            <Col span={8}>
+                <Card
+                    hoverable
+                    style={{
+                        width: 240,
+                    }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta title="Math Contest" description="12th January" />
+                </Card>
+            </Col>
+        </Row>
+        <br />
     </div >
 );
 export default Conto;
