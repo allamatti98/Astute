385b84b2bd8ce4547bcefb51371c911fa6d88057
allamatti98 <dayanallamatti98@gmail.com>
2022-11-06 17:48:38 +0300

    FAQ updates

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -1,21 +1,26 @@
 import { Collapse } from 'antd';
 import React from 'react';
 const { Panel } = Collapse;
-const text = `
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.
-`;
+
 const FrequentQuestions = () => (
     <Collapse defaultActiveKey={['1']} ghost>
-        <Panel header="This is panel header 1" key="1">
-            <p>{text}</p>
+        <Panel header="How can I reset my password" key="1">
+            <p>Well, you can request the H.O.D for assistance. Or email the Systems Administrator on joseph19@gmail.com</p>
         </Panel>
-        <Panel header="This is panel header 2" key="2">
-            <p>{text}</p>
+        <Panel header="What are the perks of participating in Contests?" key="2">
+            <p>Participating in contests earns you badges and connections.</p>
         </Panel>
-        <Panel header="This is panel header 3" key="3">
-            <p>{text}</p>
+        <Panel header="I can't see assignments set by the teachers?" key="3">
+            <p>Please contact the systems administrator as soon as you can.</p>
+        </Panel>
+        <Panel header="Is there a way I can contribute to the community database?" key="4">
+            <p>That is very possible, everyone is welcome to contribute to the community.</p>
+        </Panel>
+        <Panel header="What are the perks of participating in Contests" key="5">
+            <p>Participating in contests earns you badges and connections</p>
+        </Panel>
+        <Panel header="I can't see assignments set by the teachers" key="6">
+            <p>Please contact the systems administrator as soon as you can.</p>
         </Panel>
     </Collapse>
 );
