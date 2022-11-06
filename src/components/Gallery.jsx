6be132b8ef2d2d23683d1925533db1202c634d3f
allamatti98 <dayanallamatti98@gmail.com>
2022-11-06 21:18:44 +0300

    Gallery images co,ing through

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,18 +1,79 @@
-import { Image, Col, Row } from 'antd';
+import { Card, Col, Row } from 'antd';
 import React from 'react';
 const MultipleImages = () => (
+
     <div>
         <Row gutter={16}>
             <Col span={8}>
-                <img width={200} src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg" />
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://image.winudf.com/v2/image1/Y29tLkdvb2R3YWxscGFwZXJzLmltYWdlcy5NYXRoV2FsbHBhcGVyc19zY3JlZW5fMF8xNjE0MTI3MTM1XzAzMw/screen-0.jpg?fakeurl=1&type=.webp" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://img.freepik.com/premium-vector/astronaut-relaxing-space_27668-489.jpg?w=360" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://w0.peakpx.com/wallpaper/525/221/HD-wallpaper-biology-3-d-science.jpg" />
             </Col>
         </Row>
-        <Col span={8}>
-            <img
-                width={200}
-                src="https://gw.alipayobjects.com/zos/antfincdn/aPkFc8Sj7n/method-draw-image.svg"
-            />
-        </Col>
+        <br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://image.winudf.com/v2/image1/Y29tLkdvb2R3YWxscGFwZXJzLmltYWdlcy5NYXRoV2FsbHBhcGVyc19zY3JlZW5fMF8xNjE0MTI3MTM1XzAzMw/screen-0.jpg?fakeurl=1&type=.webp" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://img.freepik.com/premium-vector/astronaut-relaxing-space_27668-489.jpg?w=360" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://w0.peakpx.com/wallpaper/525/221/HD-wallpaper-biology-3-d-science.jpg" />
+            </Col>
+        </Row>
+        <br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://image.winudf.com/v2/image1/Y29tLkdvb2R3YWxscGFwZXJzLmltYWdlcy5NYXRoV2FsbHBhcGVyc19zY3JlZW5fMF8xNjE0MTI3MTM1XzAzMw/screen-0.jpg?fakeurl=1&type=.webp" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://img.freepik.com/premium-vector/astronaut-relaxing-space_27668-489.jpg?w=360" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://w0.peakpx.com/wallpaper/525/221/HD-wallpaper-biology-3-d-science.jpg" />
+            </Col>
+        </Row>
+        <br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://image.winudf.com/v2/image1/Y29tLkdvb2R3YWxscGFwZXJzLmltYWdlcy5NYXRoV2FsbHBhcGVyc19zY3JlZW5fMF8xNjE0MTI3MTM1XzAzMw/screen-0.jpg?fakeurl=1&type=.webp" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://img.freepik.com/premium-vector/astronaut-relaxing-space_27668-489.jpg?w=360" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://w0.peakpx.com/wallpaper/525/221/HD-wallpaper-biology-3-d-science.jpg" />
+            </Col>
+        </Row>
+        <br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://image.winudf.com/v2/image1/Y29tLkdvb2R3YWxscGFwZXJzLmltYWdlcy5NYXRoV2FsbHBhcGVyc19zY3JlZW5fMF8xNjE0MTI3MTM1XzAzMw/screen-0.jpg?fakeurl=1&type=.webp" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://img.freepik.com/premium-vector/astronaut-relaxing-space_27668-489.jpg?w=360" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://w0.peakpx.com/wallpaper/525/221/HD-wallpaper-biology-3-d-science.jpg" />
+            </Col>
+        </Row>
+        <br />
+        <Row gutter={16}>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://image.winudf.com/v2/image1/Y29tLkdvb2R3YWxscGFwZXJzLmltYWdlcy5NYXRoV2FsbHBhcGVyc19zY3JlZW5fMF8xNjE0MTI3MTM1XzAzMw/screen-0.jpg?fakeurl=1&type=.webp" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://img.freepik.com/premium-vector/astronaut-relaxing-space_27668-489.jpg?w=360" />
+            </Col>
+            <Col span={8}>
+                <img style={{ height: "300px", width: "100%", objectFit: "cover" }} alt="example" src="https://w0.peakpx.com/wallpaper/525/221/HD-wallpaper-biology-3-d-science.jpg" />
+            </Col>
+        </Row>
     </div>
 );
 export default MultipleImages;
